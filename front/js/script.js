bd6f05853acfe7437774fb9471f374d827263fc2
Anthony77151 <72447583+Anthony77151@users.noreply.github.com>
2022-05-30 14:14:06 +0200

    end products page

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,11 @@
 fetch("http://localhost:3000/api/products")
-    .then((res) => res.json())
+    .then((response) => response.json())
     .then((data) => addProducts(data))
 
+// fonction pour ajouté un produit avec les différentes infos nécessaire
 function addProducts(data) {
     
+    // loop pour réccuperer tout les produits avec nos infos
     for (let i = 0; i < data.length; i++) {
         const id = data[i]._id
         const imageUrl = data[i].imageUrl
@@ -25,25 +27,27 @@ function addProducts(data) {
     }
 }
 
+// fonction pour créer une ancre menant à l'id d'un produit lors du clique
 function makeAnchor(id) {
     const anchor = document.createElement("a")
     anchor.href = "./product.html?id=" + id
     return anchor
 }
 
+// fonction ajoutant nos ancres et articles au niveau de la section items de notre index
 function appendChildren(anchor, article) {
     const items = document.querySelector("#items")
-    if (items != null) {
-        items.appendChild(anchor)
-        anchor.appendChild(article)
-    }
+    items.appendChild(anchor)
+    anchor.appendChild(article)
 }
 
+// fonction pour créer un artcile
 function makeArticle() {
     return document.createElement("article")
 
 }
 
+// fonction pour créer une image et allant chercher l'url et l'atl de notre produit
 function makeImage(imageUrl, altTxt) {
     const image = document.createElement("img")
     image.src = imageUrl
@@ -51,6 +55,7 @@ function makeImage(imageUrl, altTxt) {
     return image
 }
 
+// fonction pour créer notre h3 qui reprendra le nom de notre produit
 function makeTitle(name) {
     const h3 = document.createElement("h3")
     h3.textContent = name
@@ -58,6 +63,7 @@ function makeTitle(name) {
     return h3
 }
 
+// fonction pour créer un paragraphe qui reprend la description de notre produit
 function makeText(description) {
     const p = document.createElement("p")
     p.textContent = description
