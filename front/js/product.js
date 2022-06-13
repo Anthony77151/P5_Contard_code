fc021f5fbd3990275e6ff55b1bb28bbc55dc57b9
Anthony77151 <72447583+Anthony77151@users.noreply.github.com>
2022-06-13 09:53:58 +0200

    reworked code and start of the basket part

--- front/js/product.js
+++ front/js/product.js
@@ -1,120 +1,111 @@
-// requête qui récupère notre produit dans l'url grace à son id et l'envoie à la fonction handleData
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString)
-const id = urlParams.get("id")
+const queryString = window.location.search; // Return l'URL actuelle sous forme de string
+const urlParams = new URLSearchParams(queryString); // searchParams fonctionne avec une url
+const id = urlParams.get("id"); // Récup' la partie ID du lien
 
-// on va chercher les informations relatifs au produit en fonction de son ID
+
 fetch(`http://localhost:3000/api/products/${id}`)
-.then((response) => response.json())
-.then((response) => handleData(response))
+    .then((response) => response.json())
+    .then((data) => {
+        document.title = data.name;
+        const { colors, name, price, description, imageUrl, altTxt } = data;
 
-
-function handleData(kanap) {
-    // change le nom de la page produit par le nom du produit
-    document.title = kanap.name
-
-    // const colors = kanap.colors
-    // const name = kanap.name
-    // const price = kanap.price
-    // const imageUrl = kanap.imageUrl
-    // const description = kanap.description
-    // const altTxt = kanap.altTxt
-
-    // même chose que dessus mais écris de façon déstructurer
-    const { 
-        colors, 
-        name, 
-        price, 
-        imageUrl, 
-        description, 
-        altTxt 
-    } = kanap
-
-    // créer les éléments de notre produit
-    makeProductImage(imageUrl, altTxt)
-    makeProductTitle("h1", name)
-    makePrice(price)
-    makeProductDescription(description)
-    choseColors(colors)
-}
+        // créer les éléments de notre produit
+        makeProductImage(imageUrl, altTxt);
+        makeProductTitle("h1", name);
+        makePrice(price);
+        makeProductDescription(description);
+        choseColors(colors);
+        const bouton = document.querySelector("#addToCart");
+        bouton.addEventListener("click", function () { // Ajoute l'article dans le panier
+            const color = document.querySelector("#colors").value;
+            const quantity = document.querySelector("#quantity").value;
+            if (color === "" || color == null || quantity == null || quantity == 0 || quantity == 101) { // Si l'utilisateur n'a pas choisi de couleur ou de quantité
+                const alert = document.createElement("div");
+                alert.classList.add("alert");
+                alert.textContent = "Veuillez choisir une couleur et une quantité valide";
+                const parent = document.querySelector("#addToCart");
+                parent.appendChild(alert);
+                setTimeout(function () {
+                    alert.remove();
+                }
+                    , 1000)
+            }
+            else if (localStorage.getItem("product_list")) {  // Si le LS contient déjà la clé "product_list"
+                let tabProduct = JSON.parse(localStorage.getItem(`product_list`)); // Récup le contenu du LS en format JSON (il est en string de base)
+                addToLocalStorage(tabProduct, data, productInfo(data)); //productInfo retourne un objet 
+            }
+            else { // Si le LS ne contient pas la clé "product_list"
+                let tabProduct = []; // je créé le tableau qui servira de valeur à cette clé
+                addToLocalStorage(tabProduct, data, productInfo(data));
+            }
+        })
+    });
 
 // fonction créant l'image du produit et la place dans la class "item__img"
 function makeProductImage(imageUrl, altTxt) {
-    const image = makeImage(imageUrl, altTxt)
-    const parent = document.querySelector(".item__img")
-    parent.appendChild(image)
+    const image = makeImage(imageUrl, altTxt);
+    const parent = document.querySelector(".item__img");
+    parent.appendChild(image);
 }
 
 // fonction pour créer un titre qui reprendra le nom de notre produit
 function makeProductTitle(kind, name) {
-    const title = makeTitle(kind, name)
-    const parent = document.querySelector("#title")
-    parent.appendChild(title)
+    const title = makeTitle(kind, name);
+    const parent = document.querySelector("#title");
+    parent.appendChild(title);
 }
 
 // fonction pour créer un prix qui reprendra le prix de notre produit
 function makePrice(price) {
-    const productPrice = document.createElement("span")
-    productPrice.textContent = price
-    const parent = document.querySelector("#price")
-    parent.appendChild(productPrice)
+    const productPrice = document.createElement("span");
+    productPrice.textContent = price;
+    const parent = document.querySelector("#price");
+    parent.appendChild(productPrice);
 }
-
 
 // fonction pour créer un paragraphe qui reprendra la description de notre produit
 function makeProductDescription(description) {
-    const productDescription = makeDescription(description)
-    const parent = document.querySelector("#description")
-    parent.appendChild(productDescription)
+    const productDescription = makeDescription(description);
+    const parent = document.querySelector("#description");
+    parent.appendChild(productDescription);
 }
 
 // fonction pour créer dans le select des options avec les différentes couleurs du produit
 function choseColors(colors) {
-    const select = document.querySelector("#colors")
-
+    const select = document.querySelector("#colors");
     // boucle pour créer les options et récupérer les différentes couleurs
     for (let i = 0; i < colors.length; i++) {
-        const option = document.createElement("option")
-        option.value = colors[i]
-        option.textContent = colors[i]
-        select.appendChild(option)
+        const option = document.createElement("option");
+        option.value = colors[i];
+        option.textContent = colors[i];
+        select.appendChild(option);
     }
 }
 
-// variable afin de vérifier si la couleur et la quantité sont valides lors du clique sur le bouton "ajouter au panier"
-const addToCartButton = document.querySelector("#addToCart")
-if (addToCartButton != null) {
-    addToCartButton.addEventListener("click", function(event) {
-        const color = document.querySelector("#colors").value
-        const quantity = document.querySelector("#quantity").value
-        // si la couleur et la quantité ne sont pas validé on affiche une alerte
-        if (color === "" || color == null || quantity == null || quantity == 0 || quantity == 101) {
-            alert("Veuillez selectionner une couleur et une quantité")
-            return
-        }
-        // si tout est valide on ajoute notre produit dans le localStorage
-        else {
-            // on créer une variable qui contiendra les données du produit
-            const kanap = {
-                id : id,
-                color : color,
-                quantity : Number(quantity),
-            }
-            
-            localStorage.setItem(id, JSON.stringify(kanap))
-            window.location.href = "cart.html"
-        }
-    })
-}
+// Fonction qui retourne un objet contenant les infos du produit (id, qty, color)
+function productInfo(dataAPI) {
+    return productObject = { // Un objet sera donc retourné
+        id: dataAPI._id,
+        qty: parseInt(document.querySelector("#quantity").value), // parseInt : Convert type str into type nbr
+        color: document.querySelector("#colors").value
+    };
+};
 
-// function addKanap(product) {
-//     const kanap = getKanap()
-//     const foundProduct = kanap.find(p => p.id == product.id)
-//     if(foundProduct != undefined) {
-//         foundProduct.quantity++
-//     } else {
-//         product.quantity = 1
-//         kanap.push(product)
-//     }
-//     saveKanap(kanap)
-// }
+// Fonction qui met à jour le LS
+function saveBasket(key, tab) {
+    localStorage.setItem(key, JSON.stringify(tab)); // json.stringify convertit une valeur JS en chaîne JSON (essentiel pour stocker dans le LS)
+};
+
+// Ajoute l'article dans le panier et s'il est déjà présent, actualise sa quantité
+function addToLocalStorage(objLocStorage, tabData, productData) {
+    let foundProduct = objLocStorage.find(elementInLS => elementInLS.id === productData.id && elementInLS.color === productData.color); // Compare l'ID et la couleur du produit à ajouter à ceux des produits dans le LS
+    if (foundProduct) { // S'ils sont identiques (if(foundProduct != undifined))
+        foundProduct.qty += productData.qty; // Update qty 
+        alert(`La quantité de votre article a été actualisée !`);
+    }
+    if (foundProduct == undefined) { // Sinon, si l'ID et la couleur sont différents 
+        objLocStorage.push(productInfo(tabData)); // Je push mon nouvel objet dans mon tableau du LS
+        alert("L'article a bien été ajouté dans votre panier.");
+    }
+    saveBasket("product_list", objLocStorage); // Je sauvegarde mon tableau dans le LS
+};
--- front/js/script.js
+++ front/js/script.js
@@ -1,78 +1,74 @@
 // on va chercher les informations de notre API avec fetch()
 fetch("http://localhost:3000/api/products")
     .then((response) => response.json())
-    .then((data) => addProducts(data))
-    .catch((error) => {
-        const messageError = document.createElement("h3")
-        messageError.textContent = "Erreur lors de la récupération des données"
-        const parent = document.querySelector("#items")
-        parent.appendChild(messageError)
+    .then((data) => {
+        // loop pour réccuperer tout les produits avec nos infos
+        for (let i = 0; i < data.length; i++) {
+            const anchor = makeAnchor(data[i]._id);
+            const article = makeArticle();
+            const image = makeImage(data[i].imageUrl, data[i].altTxt);
+
+            const title = makeTitle("h3", data[i].name);
+            title.classList.add("productName");
+
+            const p = makeDescription(data[i].description);
+            p.classList.add("productDescription");
+
+            // ajout des éléments créer dans leurs parents
+            article.appendChild(image);
+            article.appendChild(title);
+            article.appendChild(p);
+            appendChildren(anchor, article);
+        }
     })
-
-// fonction pour ajouté un produit avec les différentes infos nécessaire
-function addProducts(data) {
-    
-    // loop pour réccuperer tout les produits avec nos infos
-    for (let i = 0; i < data.length; i++) {
-        const anchor = makeAnchor(data[i]._id)
-        const article = makeArticle()
-        const image = makeImage(data[i].imageUrl, data[i].altTxt)
-
-        const title = makeTitle("h3", data[i].name);
-        title.classList.add("productName")
-        
-        const p = makeDescription(data[i].description)
-        p.classList.add("productDescription")
-    
-        // ajout des éléments créer dans leurs parents
-        article.appendChild(image)
-        article.appendChild(title)
-        article.appendChild(p)
-        appendChildren(anchor, article)
-    }
-}
+    .catch((error) => {
+        const messageError = document.createElement("h3");
+        messageError.textContent = "Erreur lors de la récupération des données";
+        const parent = document.querySelector("#items");
+        parent.appendChild(messageError);
+    })
 
 // fonction ajoutant nos ancres et articles au niveau de la section items de notre index
 function appendChildren(anchor, article) {
-    const items = document.querySelector("#items")
-    items.appendChild(anchor)
-    anchor.appendChild(article)
+    const items = document.querySelector("#items");
+    items.appendChild(anchor);
+    anchor.appendChild(article);
 }
 
 // fonction pour créer une ancre menant à l'id d'un produit lors du clique
 function makeAnchor(id) {
-    const anchor = document.createElement("a")
-    anchor.href = "./product.html?id=" + id
+    const anchor = document.createElement("a");
+    anchor.href = "./product.html?id=" + id;
 
-    return anchor
+    return anchor;
 }
 
 // fonction pour créer un artcile
 function makeArticle() {
-    return document.createElement("article")
+    return document.createElement("article");
 }
 
 // fonction pour créer une image et allant chercher l'url et l'atl de notre produit
 function makeImage(imageUrl, altTxt) {
-    const image = document.createElement("img")
-    image.src = imageUrl
-    image.alt = altTxt
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    image.alt = altTxt;
 
-    return image
+    return image;
 }
 
 // fonction pour créer un titre qui reprendra le nom de notre produit
 function makeTitle(kind, name) {
-    const title = document.createElement(kind)
-    title.textContent = name
+    const title = document.createElement(kind);
+    title.textContent = name;
 
-    return title
+    return title;
 }
 
 // fonction pour créer un paragraphe qui reprend la description de notre produit
 function makeDescription(description) {
-    const p = document.createElement("p")
-    p.textContent = description
+    const p = document.createElement("p");
+    p.textContent = description;
 
-    return p
+    return p;
 }
